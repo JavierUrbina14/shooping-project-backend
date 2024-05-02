77d5736239cc750b7b7de55dc38183a5c363372b
Javier Torres <javier.torres@ltec.cl>
2024-05-01 22:15:30 -0400

    Actualizar contraseña de usuario Javier Urbina en la semilla de usuarios

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -6,8 +6,6 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {id: '8c7d1861-8f5d-4b88-885d-9a2727e0c047', firstname: 'lucia', lastname: 'reeds', email: 'lucia@correo.com', password: '1234'},
-    {id: '4c46512d-b62f-4277-8f2f-a44c67c36880', firstname: 'Javier', lastname: 'Urbina', email: 'javier@correo.com', password: '3210'},
-    {id: 'ead8ba59-1115-42e8-a482-932a48420593', firstname: 'Joe', lastname: 'Doe', email: 'joedoe@correo.com', password: 'asdf'}
+    {id: '4c46512d-b62f-4277-8f2f-a44c67c36880', firstname: 'Javier', lastname: 'Urbina', email: 'jav.torresu@gmail.com', password: '$2a$10$X.G59TO/X9LHchg0UOz7Zu8LSfyifTQhyTPloy.eRdj.GFo7DGQEu'},
   ]);
 };
